55cdd8a614faf02696c704cbfccceda8b0c71c88
try-again34 <try_again34@tuta.io>
2017-12-18 02:54:40 +0100

    Commit from Idea

--- chapter_001/src/main/java/ru/job4j/package-info.java
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -1,8 +1,7 @@
 ﻿/**
-* Package for calculate task.
-*
-* @author George Almazov (try_again34@tuta.io)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
+ * Calculate
+ *
+ * @author George Almazov (try_again34@tuta.io)
+ * @version $Id$
+ * @since 0.1
+ */
--- src/ru/job4j/package-info.java
+++
@@ -1,8 +0,0 @@
-﻿/**
-* Package for calculate task.
-*
-* @author George Almazov (try_again34@tuta.io)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j;
