c579b8836cf2a25c45db6922b1b83d6631fcc0cb
try-again34 <try_again34@tuta.io>
2017-12-25 16:06:26 +0100

    2.3. Элементарный калькулятор

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,42 @@
+///*
+//public class Calculator {
+//	double result;
+//	double first = 23.4;
+//	double second = 15.7;
+//сложение
+//	//сложение
+//	public void add(double first, double second) {
+//		this.result = first + second;
+//	}
+//
+//	public double getResult() {
+//		return this.result;
+//	}
+//
+//	//вычитание
+//	public void subtract(double first, double second) {
+//		this.result = first - second;
+//	}
+//
+//	public double getResult() {
+//		return this.result;
+//	}
+//
+//	//деление
+//	public void div(double first, double second) {
+//		this.result = first / second;
+//	}
+//
+//	public double getResult() {
+//		return this.result;
+//	}
+//
+//	//умножение
+//	public void multiple(double first, double second) {
+//		this.result = first * second;
+//	}
+//
+//	public double getResult() {
+//		return this.result;
+//	}
+//}*/
---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,32 @@
+package ru.job4j.condition;
+
+/**
+	* @author George Almazov (try_again34@tuta.io)
+	* @version $Id$
+	* @since 0.1
+	*/
+public class Point {
+	private int x;
+	private int y;
+	
+	public Point(int x, int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
+	public double distanceTo(Point that) {
+		Point a = this;
+		Point b = that;
+		System.out.println("x1 = " + a.x);
+		System.out.println("y1 = " + a.y);
+		System.out.println("x2 = " + b.x);
+		System.out.println("y2 = " + b.y);
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		Point a = new Point(0, 1);
+		Point b = new Point(2, 5);
+	}
+		}
+
