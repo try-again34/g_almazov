09642a9fccdc5ce800c683873e96f784cbc26d76
try-again34 <try_again34@tuta.io>
2017-12-26 14:35:57 +0100

    Point

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -8,16 +8,23 @@ package ru.job4j.condition;
 public class Point {
 	private int x;
 	private int y;
-	
+
 	public Point(int x, int y) {
-		this.x=x;
-		this.y=y;
+		this.x = x;
+		this.y = y;
 	}
 
 	public double distanceTo(Point that) {
-		return Math.sqrt(
-				Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
-		);
+		Point a = this;
+		Point b = that;
+		System.out.println("x1 = " + a.x);
+		System.out.println("y1 = " + a.y);
+		System.out.println("x2 = " + b.x);
+		System.out.println("y2 = " + b.y);
+		return -1;
+//		return Math.sqrt(
+//				Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+//		);
 	}
 
 	public static void main(String[] args) {
