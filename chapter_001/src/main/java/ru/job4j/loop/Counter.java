b4d63f7d045c9ec22a668c9ff50890c628813ccd
try-again34 <try_again34@tuta.io>
2018-01-23 23:11:05 +0100

    4.1. Подсчет суммы чётных чисел в диапазоне [#192]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,6 +1,15 @@
-public int add (int start, int finish) {
-	
-	
-}
+package ru.job4j.loop;
 
 public static main [String args] {
+			int start = 1;
+			int finish = 10;
+			
+public int add (int start, int finish) {
+	for (int start; finish<=10; start++);
+		int x;
+		if (start %2==0) {
+		x = start+2;
+		}
+		return x;
+	}
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+		counter.add(30);
+		x=30;
